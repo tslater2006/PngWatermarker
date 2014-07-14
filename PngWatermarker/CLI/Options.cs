28e0a42f80ebbcc2cdf73a9b16ea8f178b9ddc76
tslater2006 <tslater2006@gmail.com>
2014-07-14 13:02:30 -0500

    Started CLI application

---
+++ PngWatermarker/CLI/Options.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+namespace CLI
+{
+    class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Input file.")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output file.")]
+        public string OutputFile { get; set; }
+
+        [Option('p',"password", Required= true,HelpText = "Storage Password.")]
+        public string Password { get; set; }
+
+        [Option('e',"encrypt",Required=false, HelpText= "Enable Encryption, if this is selected you MUST provide a value for -k/--key.")]
+        public bool Encrypt { get; set; }
+
+        [Option('a',"algo",Required=true,HelpText="Symmetric Algorithm to use, valid options are AES")]
+        public string Algorithm {get;set;}
+        [Option('k',"key", Required= false,HelpText = "Encryption Key.")]
+        public string EncKey { get; set; }
+
+        [Option('r',"reedsolomon",Required=false,HelpText="Enables ReedSolomon protection.")]
+        public bool ReedSolomon { get; set; }
+
+        [OptionArray('t',"text",HelpText="Text watermark(s), each string should be wrapped in quotes and seperated by a space.")]
+        public string[]TextMarks {get;set;}
+
+        [OptionArray('f',"file", HelpText = "File watermark(s), each file should be wrapped in quotes and seperated by a space.")]
+        public string[] FileMarks { get; set; }
+
+        [OptionArray('b', "binary", HelpText = "Binary watermark(s), each one should be formatted like {34,AE,2F,4B} and should be seperated by a space.")]
+        public string[] BinaryMarks { get; set; }
+    }
+}
---
+++ PngWatermarker/CLI/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PngWatermarker;
+using PngWatermarker.Watermarks;
+
+namespace CLI
+{
+    class Program
+    {
+        static List<TextWatermark> textMarks = new List<TextWatermark>();
+        static List<BinaryWatermark> binMarks = new List<BinaryWatermark>();
+        static List<FileWatermark> fileMarks = new List<FileWatermark>();
+
+        private static int totalMarks
+        {
+            get
+            {
+                return textMarks.Count + binMarks.Count + fileMarks.Count;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            args = new string[] { "-i=a", "-k=nut", "-o=b", "-k \"coco\":\"nut\"", "-p=3" };
+
+            Watermark finalMark = null;
+
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // process any text watermarks
+                foreach (var x in options.TextMarks)
+                {
+                    TextWatermark t = new TextWatermark(x.Substring(1, x.Length - 2)); // new TextWatermark w/o beginning and ending quotes
+                    textMarks.Add(t);
+                }
+
+                // process any file watermarks
+                foreach(var x in options.FileMarks)
+                {
+                    FileWatermark f = new FileWatermark(x);
+                    fileMarks.Add(f);
+                }
+
+                // process any binary watermarks
+                foreach(var x in options.BinaryMarks)
+                {
+                    string[] parts = x.Replace("{", "").Replace("}", "").Split(',');
+                    byte[] data = new byte[parts.Length];
+
+                    for(var y = 0; y < parts.Length; y++)
+                    {
+                        data[y] = Convert.ToByte(parts[y], 16);
+                    }
+
+                    BinaryWatermark b = new BinaryWatermark(data);
+                    binMarks.Add(b);
+                }
+
+
+
+                // Values are available here
+                int i = 1;
+                i++;
+            }
+            else
+            {
+                var b = CommandLine.Text.HelpText.AutoBuild(options);
+
+                Console.WriteLine(b.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
